68337af84a668ab10df3001703bc435e8ddee7ed
White Tiger <dayley.zimmermann@gmail.com>
2022-07-11 12:53:34 -0400

    set up new question game to refresh javascript skills

---
+++ app.js
@@ -0,0 +1,37 @@
+const scoreDisplay = document.getElementById("score-display")
+const questionDisplay = document.getElementById("question-display")
+
+const questions = [
+  {
+    quiz: ['value', 'estimate', 'evaluate'],
+    options: ['jury','assess'],
+    correct: 2
+  },
+  {
+    quiz: ['value', 'estimate', 'evaluate'],
+    options: ['jury','assess'],
+    correct: 2
+  },
+  {
+    quiz: ['value', 'estimate', 'evaluate'],
+    options: ['jury','assess'],
+    correct: 2
+  },
+  {
+    quiz: ['value', 'estimate', 'evaluate'],
+    options: ['jury','assess'],
+    correct: 2
+  },
+
+]
+
+let score = 0
+scoreDisplay.textContent = score
+
+function populateQuestions() {
+  questions.forEach((question) => {
+    const questionBox = document.createElement('div')
+    questionBox.classList.add('question-box')
+    questionDisplay.append(questionBox)
+  })
+}
