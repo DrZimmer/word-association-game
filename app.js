fb0bb476b8f429bcaa989d89fb16c94c4b2491f3
White Tiger <dayley.zimmermann@gmail.com>
2022-07-18 14:38:59 -0400

    added individual questions with for each looping through options and counting score for correct answer. Also included disabling after clicking the buttons

--- app.js
+++ app.js
@@ -26,15 +26,61 @@ const questions = [
 ]
 
 let score = 0
+let clicked = []
+
+
 scoreDisplay.textContent = score
 
 function populateQuestions() {
+  //everything
   questions.forEach(question => {
     const questionBox = document.createElement('div')
     questionBox.classList.add('question-box')
-    questionBox.innerHTML = "BOX!"
+
+    const logoDisplay = document.createElement('h1')
+    logoDisplay.textContent = "<=>"
+    questionBox.append(logoDisplay)
+
+    //second for each loop creating the quiz p elements/textContent
+    question.quiz.forEach(tip => {
+      const tipText = document.createElement('p')
+      tipText.textContent = tip
+      questionBox.append(tipText)
+    })
+
+    const questionButtons = document.createElement('div')
+    questionButtons.classList.add('question-buttons')
+    questionBox.append(questionButtons)
+
+    question.options.forEach((option, optionIndex) => {
+      const questionButton = document.createElement('button')
+      questionButton.classList.add('question-button')
+      questionButton.textContent = option
+
+      questionButton.addEventListener('click', () => checkanswer(questionButton, option, optionIndex + 1, question.correct))
+
+      questionButtons.append(questionButton)
+    })
+
+
+
     questionDisplay.append(questionBox)
   })
 }
 
 populateQuestions()
+
+function checkanswer(questionButton, option, optionIndex, correctAnswer) {
+  console.log("option ", option)
+  console.log("option index ", optionIndex)
+
+  if(optionIndex === correctAnswer) {
+    score++
+    scoreDisplay.textContent = score
+  } else {
+    score--
+    scoreDisplay.textContent = score
+  }
+  clicked.push(option)
+  questionButton.disabled = clicked.includes(option)
+}
